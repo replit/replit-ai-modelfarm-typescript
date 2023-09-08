929ca1049ffc0982ee19a532c25fc6dde13097f2
masad-frost <farismasad@gmail.com>
2023-09-08 00:18:06 +0000

    More extensible return types

--- src/embedding.test.ts
+++ src/embedding.test.ts
@@ -10,7 +10,7 @@ test('embedding', async () => {
   });
 
   expect(completion.error).toBeFalsy();
-  expect(completion.value).toMatchObject({
+  expect(completion.value?.embeddings).toMatchObject({
     values: expect.arrayContaining([expect.any(Number)]),
     truncated: expect.any(Boolean),
   });
